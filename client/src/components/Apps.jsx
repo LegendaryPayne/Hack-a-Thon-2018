ee592a2e62536e8cc9774bb3fe0dea6d5175ced3
LegendaryPayne <legendarypayne@gmail.com>
2018-08-19 10:09:32 -0500

    final adds

--- client/src/components/Apps.jsx
+++ client/src/components/Apps.jsx
@@ -1,15 +1,23 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Navigation from './Navigation';
 import Home from './Home'
 
 class Apps extends Component {
+    constructor(props) {
+        super(props);
+    }
 
     render() {
         return (
             <Router>
                 <Fragment>
+                    <Navigation />
+
                     <Switch>
-                        <Route exact path="/" component={Home} />
+                        <Route exact path="/signup" component={Signup}/> 
+                        <Route exact path="/login" component={Login}/> 
+                        <Route exact path="/" component={Home}/> 
                     </Switch>
                 </Fragment>
             </Router>
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,49 +1,29 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-class Home extends Component {
+class Navigation extends Component {
 
     constructor(props) {
         super(props);
     }
     
+
+initMap() {
+        let map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+
+
     render() {
+        this.initMap()
         return (
-<React.Fragment>
-            <form className="form-signin">
-
-              <img className="mb-4" src="../../assets/brand/bootstrap-solid.svg" alt="" width="72" height="72"/>
-
-              <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-
-              <label for="inputEmail" className="sr-only">Email address</label>
-
-              <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required="" autofocus=""/>
-
-              <label for="inputPassword" className="sr-only">Password</label>
-
-              <input type="password" id="inputPassword" className="form-control" placeholder="Password" required=""/>
-
-              <div className="checkbox mb-3">
-
-                <label>
-
-                  <input type="checkbox" value="remember-me"/> Remember me
-
-                </label>
-
-              </div>
-
-              <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-
-              <p className="mt-5 mb-3 text-muted">© 2017-2018</p>
-
-            </form>
-            
+            <React.Fragment>
+<div id="map" style={{height: "100px"}}></div>
             </React.Fragment>
-         
-        );
+        )
     }
 }
 
-export default Home;
+export default Navigation;
