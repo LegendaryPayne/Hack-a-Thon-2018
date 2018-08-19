2c8f36f0cd5d8c465d09265cae970a70a4c6b1fc
LegendaryPayne <legendarypayne@gmail.com>
2018-08-18 23:18:31 -0500

    add sass

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import * as mdc from 'material-components-web'
 
-// import {MDCTextField} from '@material/textfield';
-
-// import {MDCRipple} from '@material/ripple';
-
-
-// new MDCRipple(document.querySelector('.cancel'));
-// new MDCRipple(document.querySelector('.next'));
-
-// const username = new MDCTextField(document.querySelector('.username'));
-// const password = new MDCTextField(document.querySelector('.password'));
 
 ReactDOM.render(<App />, document.getElementById('root'));
